506fe33d3cd70e691920e8ab0408f05e5f428c2a
Zehua Tian <zehuat@Zehuas-MacBook-Pro.local>
2015-10-08 02:12:08 +0200

    add Chair

--- app/js/components/Analysor.jsx
+++ app/js/components/Analysor.jsx
@@ -5,7 +5,8 @@ var React = require('react'),
     AppActionCreators = require('../actions/AppActionCreators'),
     Switches = require('./Switches'),
     Server = require('./Server'),
-    PLC = require('./PLC');
+    PLC = require('./PLC'),
+    Chair = require('./Chair');
 
 var Router = require('react-router');
 var RouteHandler = Router.RouteHandler;
@@ -29,7 +30,7 @@ var Analysor = React.createClass({
         <RouteHandler />
         <div className="row" style={{"textAlign": "center"}}>
           <div className="1" style={{"textAlign": "center"}}>
-            <svg version="1.1" id="to_1" x="0px" y="0px" width="90%" height="600" preserveAspectRatio="xMidYMid meet" style={{"textAlign": "center", "borderStyle":"solid","borderWidth":"2px","borderColor":"black","backgroundColor":"white"}} viewBox="0 0 900 600">
+            <svg version="1.1" id="to_1" x="0px" y="0px" width="90%" height="600" preserveAspectRatio="xMidYMid meet" onClick={this._onClick} style={{"cursor":"pointer", "textAlign": "center", "borderStyle":"solid","borderWidth":"2px","borderColor":"black","backgroundColor":"white"}} viewBox="0 0 900 600">
               <line x1="450" y1="500" x2="450" y2="10" style={{"stroke":"rgb(255,0,0)","strokeWidth":"2","cursor":"pointer"}} onClick={this._onClick} />
               <line x1="286" y1="104" x2="450" y2="104" style={{"stroke":"rgb(255,0,0)","strokeWidth":"2","cursor":"pointer"}} onClick={this._onClick} />
               <line x1="614" y1="204" x2="450" y2="204" style={{"stroke":"rgb(255,0,0)","strokeWidth":"2","cursor":"pointer"}} onClick={this._onClick} />
@@ -37,7 +38,7 @@ var Analysor = React.createClass({
               <Switches id = "1" transform="translate(386, 436)"/> 
               <PLC id = "plc1" transform="translate(222, 40)" />
               <Server id = "3" transform="translate(550, 140)" />
-              <PLC id = "chair1" transform="translate(550, 336)" />
+              <Chair id = "chair1" transform="translate(550, 336)" />
             </svg>
           </div>
         </div>  
